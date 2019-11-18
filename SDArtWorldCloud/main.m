5834c254c55382da6546d16602acbfb7e56ca30b
yansha <1224894560@qq.com>
2019-11-18 14:17:08 +0800

    新建项目

--- SDArtWorldCloud/main.m
+++ SDArtWorldCloud/main.m
@@ -1,19 +1,16 @@
 //
 //  main.m
-//  SDArtWorldCloud
+//  VagaryArtWorldProject
 //
-//  Created by metis on 2019/11/18.
-//  Copyright © 2019 Sudo. All rights reserved.
+//  Created by metis2017 on 2019/7/11.
+//  Copyright © 2019年 metis2017. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    NSString * appDelegateClassName;
     @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
-    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
