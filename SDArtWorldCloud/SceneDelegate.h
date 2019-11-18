fd19d22aca6cf37bc43fbacfd7731d77386a0d04
yansha <1224894560@qq.com>
2019-11-18 13:28:44 +0800

    Initial Commit

---
+++ SDArtWorldCloud/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  SDArtWorldCloud
+//
+//  Created by metis on 2019/11/18.
+//  Copyright © 2019 Sudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
